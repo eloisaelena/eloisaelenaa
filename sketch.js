23a845725808878c4398faae57296e3b81596069
eloisaelena <welter.eloisa@escola.pr.gov.br>
2024-10-07 11:42:36 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,32 @@
-let palavra;
+let cor;
+let circuloX; // horizontal
+let circuloY; // vertical
 
 function setup() {
   createCanvas(400, 400);
-
-  palavra = palavraAleatoria();
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
   
-}
-
-function palavraAleatoria() {
-  
-  let palavras = ["como amo vocẽ", "você é incrivel", "vou sentir falta"];
-  
-  return random(palavras);
-}
-
-function inicializaCores() {
-  background("black");
-  fill("red");
-  textSize(64);
-  textAlign(CENTER, CENTER);
-}
-
-function palavraParcial(minimo, maximo) {
-  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
-  let parcial = palavra.substring(0, quantidade);
-  return parcial;
+  circuloX = [0, 0, 0];
+  circuloY = [random(height), random(height), random(height)];
 }
 
 function draw() {
   
-  inicializaCores();
-
-  let texto = palavraParcial(0, width);
-    
-  text(texto, 200, 200);
+  fill("pink");
   
-}
-
-function modoNoturno(horario) {
-  if (horario > 18) {
-    console.log("Você precisa ligar o modo escuro!");
-  } else {
-    console.log("Modo noturno não é necessário neste momento.");
+  for(let contador in circuloX) {
+    circle(circuloX[contador], circuloY[contador], 50);    
+    circuloX[contador]+= random(0,3);
+    circuloY[contador]+= random(-3,3); 
+    
+    if(circuloX[contador] >= width){
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+    }
+  }
+  
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
   }
 }
-
-modoNoturno(15);
-modoNoturno(20);
